85a5c8c2e5517282de452857efed30080150be46
Jerry Peng <62306612+JerryPeng2020@users.noreply.github.com>
2020-08-21 20:59:02 +0800

    Update sketch.js

--- GarbageSorting_test/sketch.js
+++ GarbageSorting_test/sketch.js
@@ -182,10 +182,10 @@ function modelReady() {
     // classifier.load('./model/model.json', function() {
     //   select('#modelStatus').html('Custom Model Loaded!');
     // });
-    select('#modelStatus').html('Load Custom Model fail!');
+   // select('#modelStatus').html('Load Custom Model fail!');
 	
-    classifier.load('./model20200612/model.json', function() {
-    select('#modelStatus').html('Custom Model Loaded!');
+   // classifier.load('./model20200612/model.json', function() {
+  //  select('#modelStatus').html('Custom Model Loaded!');
     });
 	
 	
